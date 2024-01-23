4eaa4bed800e8e93946fe47e8b710cfe55a9d596
Rattapon Apiratjit <krtp.apirat@gmail.com>
2024-01-23 14:33:59 +0700

    Add NextAuth client provider

--- src/providers/index.tsx
+++ src/providers/index.tsx
@@ -1,12 +1,17 @@
 import { ReactNode } from "react";
 import { PostHogProvider } from "./posthog";
 import { Toaster } from "@/components/ui/sonner";
+import { SessionProvider } from "./nextauth";
+import { getServerAuthSession } from "@/server/modules/auth/server";
 
-export function Provider({ children }: { children: ReactNode }) {
+export async function Provider({ children }: { children: ReactNode }) {
+  const session = await getServerAuthSession();
   return (
-    <PostHogProvider>
-      {children}
-      <Toaster />
-    </PostHogProvider>
+    <SessionProvider session={session}>
+      <PostHogProvider>
+        {children}
+        <Toaster />
+      </PostHogProvider>
+    </SessionProvider>
   );
 }
--- src/server/modules/auth/server.ts
+++ src/server/modules/auth/server.ts
@@ -2,4 +2,4 @@
 import { getServerSession } from "next-auth";
 import { authOptions } from ".";
 
-export const getServerAuthSession = getServerSession(authOptions);
+export const getServerAuthSession = () => getServerSession(authOptions);
