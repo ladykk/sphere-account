d193a88709d15a332f018a7de9d50cb9e54e04f3
Rattapon Apiratjit <krtp.apirat@gmail.com>
2024-01-24 14:57:29 +0700

    Add auth router to app router

--- src/server/routers/index.ts
+++ src/server/routers/index.ts
@@ -1,9 +1,11 @@
 import { publicProcedure, createTRPCRouter } from "../trpc";
+import { authRouter } from "./auth";
 
 export const appRouter = createTRPCRouter({
   test: publicProcedure.query((opts) => {
     return "test";
   }),
+  auth: authRouter,
 });
 
 export type AppRouter = typeof appRouter;
