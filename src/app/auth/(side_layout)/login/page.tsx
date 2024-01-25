80fda5e7aa5d3ee94d33675dfa9a889324789689
Rattapon Apiratjit <krtp.apirat@gmail.com>
2024-01-25 13:16:31 +0700

    resize input elements

--- src/app/auth/(side_layout)/login/page.tsx
+++ src/app/auth/(side_layout)/login/page.tsx
@@ -16,36 +16,30 @@ export default function Page() {
   return (
     <div className="w-full space-y-6">
       <h1 className="mb-2">Login to your account</h1>
-      <h4 className="text-muted-foreground">Login in with your email:</h4>
-      <div className="space-y-6">
+      <h5 className="text-muted-foreground ">Login in with your email</h5>
+      <div className="space-y-4">
         <Input
           type="email"
-          inputSize="xl"
           prefixIcon={emailIcon}
           placeholder="Email Address"
         />
-        <Input
-          type="password"
-          inputSize="xl"
-          prefixIcon={lockKey}
-          placeholder="Password"
-        />
+        <Input type="password" prefixIcon={lockKey} placeholder="Password" />
       </div>
-      <div className="flex items-center w-full gap-11 px-2">
-        <Separator className="flex-1 bg-muted-foreground" />
-        <h4>or continue with</h4>
+      <div className="flex items-center w-full gap-8 px-2">
+        <Separator className="flex-1 bg-muted-foreground text-sm" />
+        <p>or continue with</p>
         <Separator className="flex-1 bg-muted-foreground" />
       </div>
       <div className="flex items-center gap-3">
-        <Button size="xl" variant="outline" className="flex-1 gap-2">
+        <Button variant="outline" className="flex-1 gap-2">
           <Image src={googleLogo} alt="Google" className="w-5 h-5" />
           Google
         </Button>
-        <Button size="xl" variant="outline" className="flex-1 gap-2">
+        <Button variant="outline" className="flex-1 gap-2">
           <Image src={facebookLogo} alt="Facebook" className="w-5 h-5" />
           Facebook
         </Button>
-        <Button size="xl" variant="outline" className="flex-1 gap-2">
+        <Button variant="outline" className="flex-1 gap-2">
           <Image src={lineLogo} alt="LINE" className="w-5 h-5" />
           Line
         </Button>
@@ -53,29 +47,21 @@ export default function Page() {
       <div className="flex justify-between items-center">
         <div className="flex gap-3 items-center">
           <Checkbox />
-          <Label className="text-base font-normal">Remember me</Label>
+          <Label className="font-normal">Remember me</Label>
         </div>
         <Link
           href="/auth/reset-password"
-          className={cn(
-            buttonVariants({ variant: "link" }),
-            "text-base font-normal"
-          )}
+          className={cn(buttonVariants({ variant: "link" }), "font-normal")}
         >
           Forget Password?
         </Link>
       </div>
-      <Button className="w-full my-10" size="xl">
-        Login
-      </Button>
-      <div className="flex justify-center gap-6 items-center">
+      <Button className="w-full my-8">Login</Button>
+      <div className="flex justify-center gap-6 items-center text-sm">
         <div>Don't have an account? </div>
         <Link
           href="/auth/register"
-          className={cn(
-            buttonVariants({ variant: "link" }),
-            "text-base font-normal"
-          )}
+          className={cn(buttonVariants({ variant: "link" }), "font-normal")}
         >
           Create an account
         </Link>
--- src/app/auth/(side_layout)/register/page.tsx
+++ src/app/auth/(side_layout)/register/page.tsx
@@ -13,10 +13,10 @@ import { cn } from "@/lib/utils";
 export default function page() {
   return (
     <div className="w-full">
-      <h1 className="mb-6">Register </h1>
-      <h4 className=" text-muted-foreground">
+      <h1 className="mb-4">Register </h1>
+      <h5 className="text-muted-foreground mb-4">
         Enter your information below to create your account
-      </h4>
+      </h5>
       <div className="space-y-4 mt-3">
         <div className="flex gap-2 items-center">
           <Input placeholder="First Name" />
@@ -29,39 +29,36 @@ export default function page() {
           placeholder="Confirm Password"
           prefixIcon={lockKey}
         />
-        <div className="flex items-center w-full gap-11 px-2">
+        <div className="flex items-center w-full gap-8 px-2 py-1">
           <Separator className="flex-1 bg-muted-foreground" />
-          <h4>or continue with</h4>
+          <p>or continue with</p>
           <Separator className="flex-1 bg-muted-foreground" />
         </div>
         <div className="flex items-center gap-3">
-          <Button size="xl" variant="outline" className="flex-1 gap-2">
+          <Button variant="outline" className="flex-1 gap-2">
             <Image src={googleLogo} alt="Google" className="w-5 h-5" />
             Google
           </Button>
-          <Button size="xl" variant="outline" className="flex-1 gap-2">
+          <Button variant="outline" className="flex-1 gap-2">
             <Image src={facebookLogo} alt="Facebook" className="w-5 h-5" />
             Facebook
           </Button>
-          <Button size="xl" variant="outline" className="flex-1 gap-2">
+          <Button variant="outline" className="flex-1 gap-2">
             <Image src={lineLogo} alt="LINE" className="w-5 h-5" />
             Line
           </Button>
-              </div>
-              
-          </div>
-          <Button className="w-full my-9" size="xl">
-        Register
-          </Button>
-          <div className="flex justify-center gap-6 items-center">
-              <div> Already have an account?</div>
-              <Link
-                  href="/auth/login"
-                  className={cn(
-            buttonVariants({ variant: "link" }),
-            "text-base font-normal"
-          )}>Login</Link>
-          </div>
+        </div>
+      </div>
+      <Button className="w-full my-9">Register</Button>
+      <div className="flex justify-center gap-6 items-center text-sm">
+        <div> Already have an account?</div>
+        <Link
+          href="/auth/login"
+          className={cn(buttonVariants({ variant: "link" }), "font-normal")}
+        >
+          Login
+        </Link>
+      </div>
     </div>
   );
 }
