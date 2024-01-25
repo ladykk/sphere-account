bec604db55b920e6f4a14766a08713caff25ccbb
Rattapon Apiratjit <krtp.apirat@gmail.com>
2024-01-25 15:34:49 +0700

    refactor customers schema

--- src/db/schema/customer.ts
+++ src/db/schema/customer.ts
@@ -1,43 +1,88 @@
-
-import {
-    timestamp,
-    pgTable,
-    text,
-    primaryKey,
-    integer,
-    boolean,
-    date,
-} from "drizzle-orm/pg-core";
+import { sql } from "drizzle-orm";
+import { timestamp, pgTable, text, boolean, uuid } from "drizzle-orm/pg-core";
+import { users } from "./auth";
 
 export const customers = pgTable("customer", {
-    id: text("id").notNull().primaryKey(),
-    customer_name: text("customer_name"),
-    address: text("address"),
-    zipcode: text("zipcode"),
-    tax_id: text("tax_id"),
-    branch: boolean('branch'),
-    branch_code: text("branch_code"),
-    branch_name: text("branch_name"),
-    business_type: text("business_type"),
-    //contact_id: integer("contact_id"),
-    business_name: text("business_name"),
-    contact_name: text("contact_name"),
-    email: text("email"),
-    mobile: text("mobile"),
-    bank: text("bank"),
-    account_number: text("account_number"),
-    bank_branch: text("bank_branch"),
-    account_type: text("account_type"),
-    contact_type: text("contact_type"),
-    credit_date: date("credit_date"),
-    phone: text("phone"),
-    fax_number: text("fax_number"),
-    website: text("website"),
-    shipping_address: text("shipping_address"),
-    //attachment: text("attachment"), //binary
-    notes: text("notes"),
-    create_by: text("create_by"),
-    create_on: timestamp("create_on", { mode: "date" }),
-    update_by: text("update_by"),
-    update_on: timestamp("update_on", { mode: "date" })
-})
+  id: uuid("id").notNull().primaryKey(),
+  // Information
+  name: text("name").notNull(),
+  taxId: text("tax_id"),
+  address: text("address"),
+  shipping_address: text("shipping_address"),
+  zipcode: text("zipcode"),
+  isBranch: boolean("is_branch").notNull().default(false),
+  branchCode: text("branch_code"),
+  branchName: text("branch_name"),
+  businessType: text("business_type"),
+  email: text("email"),
+  telelphoneNumber: text("telelphone_number"),
+  phoneNumber: text("phone_number"),
+  faxNumber: text("fax_number"),
+  website: text("website"),
+  notes: text("notes"),
+  createdAt: timestamp("created_at", { mode: "date" })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  createdBy: text("created_by").references(() => users.id, {
+    onDelete: "set null",
+  }),
+  updatedAt: timestamp("updated_at", { mode: "date" })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedBy: text("updated_by").references(() => users.id, {
+    onDelete: "set null",
+  }),
+  //   contact_type: text("contact_type"),
+  //   credit_date: date("credit_date"),
+});
+
+export const customerContacts = pgTable("customer_contacts", {
+  id: uuid("id").notNull().primaryKey(),
+  customerId: text("customer_id")
+    .notNull()
+    .references(() => customers.id, {
+      onDelete: "cascade",
+    }),
+  contactName: text("contact_name").notNull(),
+  email: text("email"),
+  phoneNumber: text("phone_number"),
+  createdAt: timestamp("created_at", { mode: "date" })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  createdBy: text("created_by").references(() => users.id, {
+    onDelete: "set null",
+  }),
+  updatedAt: timestamp("updated_at", { mode: "date" })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedBy: text("updated_by").references(() => users.id, {
+    onDelete: "set null",
+  }),
+});
+
+export const customerBankAccounts = pgTable("customer_bank_accounts", {
+  id: uuid("id").notNull().primaryKey(),
+  customerId: text("customer_id")
+    .notNull()
+    .references(() => customers.id, {
+      onDelete: "cascade",
+    }),
+  bank: text("bank").notNull(),
+  accountNumber: text("account_number").notNull(),
+  bankBranch: text("bank_branch").notNull(),
+  accountType: text("account_type").notNull(),
+  createdAt: timestamp("created_at", { mode: "date" })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  createdBy: text("created_by").references(() => users.id, {
+    onDelete: "set null",
+  }),
+  updatedAt: timestamp("updated_at", { mode: "date" })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedBy: text("updated_by").references(() => users.id, {
+    onDelete: "set null",
+  }),
+});
+
+// TODO: Attachment Table
--- src/db/schema/index.ts
+++ src/db/schema/index.ts
@@ -1,6 +1,8 @@
 import * as auth from "./auth";
+import * as customer from "./customer";
 
 const schema = {
   ...auth,
+  ...customer,
 };
 export default schema;
