5ff3ea1fc79407a21245707f4d8e98f19d692dcf
Rattapon Apiratjit <krtp.apirat@gmail.com>
2024-01-26 13:04:14 +0700

    add login logic to ui

--- src/app/auth/(side_layout)/layout.tsx
+++ src/app/auth/(side_layout)/layout.tsx
@@ -1,12 +1,22 @@
+"use client";
 import imgLogin from "@/asset/image/sphere-login.png";
 import Image from "next/image";
 import SphereLogo from "@/asset/image/SphereLogo.png";
+import { useEffect, useState } from "react";
+import { Spinner } from "@/components/ui/spinner";
 
 type Props = {
   children: React.ReactNode;
 };
 
-export default function LoginLayout(props: Props) {
+export default function AuthSideLayout(props: Props) {
+  const [isLoaded, setLoaded] = useState(false);
+
+  // Wait until client is ready
+  useEffect(() => {
+    setLoaded(true);
+  }, []);
+
   return (
     <section className="min-h-screen">
       <div className="flex h-screen">
@@ -20,14 +30,20 @@ export default function LoginLayout(props: Props) {
           />
         </div>
         <div className="w-[50%] flex justify-center flex-col max-w-md mx-auto">
-          <Image
-            src={SphereLogo}
-            alt="Spheresoft Account"
-            className="mb-10"
-            width={379}
-            height={56.27}
-          />
-          {props.children}
+          {!isLoaded ? (
+            <Spinner className="h-14 w-14 mx-auto" />
+          ) : (
+            <>
+              <Image
+                src={SphereLogo}
+                alt="Spheresoft Account"
+                className="mb-10"
+                width={379}
+                height={56.27}
+              />
+              {props.children}
+            </>
+          )}
         </div>
       </div>
     </section>
