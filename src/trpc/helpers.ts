28e7855a86678e975267f9f409f7082b4d675b1a
Rattapon Apiratjit <krtp.apirat@gmail.com>
2024-01-31 21:03:04 +0700

    fix build issue

--- src/trpc/helpers.ts
+++
@@ -1,8 +0,0 @@
-import { appRouter } from "@/server/routers";
-import { createServerSideHelpers } from "@trpc/react-query/server";
-import { createContext } from "./server";
-
-export const helpers = createServerSideHelpers({
-  router: appRouter,
-  ctx: await createContext(),
-});
