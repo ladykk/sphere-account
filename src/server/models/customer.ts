2b631e1cb2f5a899ef5320b2edc5bc9c8c48dd17
pao1230 <53170787+pao1230@users.noreply.github.com>
2024-02-04 21:26:19 +0700

    Create Customer API

---
+++ src/server/models/customer.ts
@@ -0,0 +1,69 @@
+import { z } from "zod"
+
+export const Customer = {
+    schemas: {
+        createCustomerInput: z.object({
+            id: z.string().min(1, "Require Customer's id").uuid("Invalid uuid"),
+            name: z.string().min(1, "Require Customer's name"),
+            detail: z.string().nullable().default(""),
+            taxId: z.string().nullable().default(""),
+            address: z.string().nullable().default(""),
+            shipping_address: z.string().nullable().default(""),
+            zipcode: z.string().nullable().default(""),
+            isBranch: z.boolean({
+                required_error: "isBranch is required",
+                invalid_type_error: "isBranch must be a boolean",
+            }),
+            branchCode: z.string().nullable().default(""),
+            branchName: z.string().nullable().default(""),
+            businessType: z.string().nullable().default(""),
+            email: z.string().nullable().default(""),
+            telelphoneNumber: z.string().nullable().default(""),
+            phoneNumber: z.string().nullable().default(""),
+            faxNumber: z.string().nullable().default(""),
+            website: z.string().nullable().default(""),
+            notes: z.string().nullable().default(""),
+            createdAt: z.date(),
+            createdBy: z.string().nullable(),
+            updatedAt: z.date(),
+            updatedBy: z.string().nullable(),
+        }),
+
+        createCustomerOutputSchema: z.object({
+            id: z.string()
+        }),
+
+        createCustomerContact: z.object({
+            id: z.string().min(1, "Require Customer Contact's id").uuid("Invalid uuid"),
+            customerId: z.string().min(1, "Require customerId").uuid("Invalid uuid"),
+            contactName: z.string().min(1, "Require Contact's name"),
+            email: z.string().nullable().default(""),
+            phoneNumber: z.string().nullable().default(""),
+            createdAt: z.date(),
+            createdBy: z.string().nullable(),
+            updatedAt: z.date(),
+            updatedBy: z.string().nullable(),
+        }),
+
+        createCustomerContactOutput: z.object({
+            id: z.string()
+        }),
+
+        createCustomerBankAccounts: z.object({
+            id: z.string().min(1, "Require project's id").uuid("Invalid uuid"),
+            customerId: z.string().min(1, "Require customerId").uuid("Invalid uuid"),
+            bank: z.string(),
+            accountNumber: z.string(),
+            bankBranch: z.string(),
+            accountType: z.string(),
+            createdAt: z.date(),
+            createdBy: z.string().nullable(),
+            updatedAt: z.date(),
+            updatedBy: z.string().nullable(),
+        }),
+
+        createCustomerBankACcounts: z.object({
+            id: z.string(),
+        })
+    }
+}
