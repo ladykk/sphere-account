892726efaa8f8817c4d507c8878def56ac0f52f1
thanawatria <thanawatria@gmail.com>
2024-02-05 17:43:07 +0700

    Customers

---
+++ src/app/app/customers/[customerId]/page.tsx
@@ -0,0 +1,416 @@
+"use client";
+
+import { DashboardFormContainer } from "@/components/layouts/dashboard";
+import { Breadcrumb } from "@/components/ui/breadcrumb";
+import { Button } from "@/components/ui/button";
+import { FileUploadDropzone } from "@/components/ui/file-upload";
+import {
+  Form,
+  FormControl,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "@/components/ui/form";
+import { Input } from "@/components/ui/input";
+import { Spinner } from "@/components/ui/spinner";
+import { Textarea } from "@/components/ui/textarea";
+import { handleTRPCFormError } from "@/lib/utils";
+import { api } from "@/trpc/react";
+import { RouterInputs } from "@/trpc/shared";
+import { Separator } from "@radix-ui/react-separator";
+import { Home, X } from "lucide-react";
+import { useParams, useRouter } from "next/navigation";
+import { useEffect, useState } from "react";
+import { useForm } from "react-hook-form";
+import { toast } from "sonner";
+
+export default function ProjectDetailPage(props) {
+  const router = useRouter();
+  const params = useParams();
+  const projectId = params.projectId as string;
+  const isCreate = projectId === "create";
+  const { children, ...fileUploadProps } = props;
+  const [isEdit, setIsEdit] = useState(false);
+  const [isDisabled, setIsDisabled] = useState(false);
+
+  const query = api.project.getProject.useQuery(projectId, {
+    enabled: !isCreate,
+    refetchOnMount: false,
+    refetchOnWindowFocus: false,
+    refetchOnReconnect: false,
+  });
+
+  const form = useForm<RouterInputs["project"]["createOrUpdateProject"]>({
+    disabled: (isCreate ? false : !isEdit) || isDisabled || query.isLoading,
+  });
+
+  const mutation = api.project.createOrUpdateProject.useMutation({
+    onSuccess: (id, variables) => {
+      router.replace(`/app/customers/${id}`);
+      form.reset(variables);
+    },
+    onError: (error) =>
+      handleTRPCFormError(error.data?.zodError, form.setError),
+    onMutate: () => setIsDisabled(true),
+    onSettled: () => setIsDisabled(false),
+  });
+
+  // Set form data if query is successful
+  useEffect(() => {
+    if (!query.data) return;
+    form.reset({
+      id: query.data.id,
+      name: query.data.name,
+      customerId: query.data.customerId,
+      detail: query.data.detail,
+    });
+  }, [query.data]);
+
+  const onSubmit = (
+    input: RouterInputs["project"]["createOrUpdateProject"]
+  ) => {
+    toast.promise(mutation.mutateAsync(input), {
+      loading: "Saving Project...",
+      success: "Project Saved",
+      error: "Failed to Save Project",
+    });
+  };
+
+  return (
+    <Form {...form}>
+      <DashboardFormContainer onSubmit={form.handleSubmit(onSubmit)}>
+        <div className="flex items-baseline gap-3 mb-10">
+          <h1>{isCreate ? "Create Customer" : "Customer Detail"}</h1>
+          {query.isLoading && <Spinner />}
+          <Breadcrumb
+            items={[
+              {
+                label: "Customers",
+                icon: Home,
+              },
+              {
+                label: "Cusomters' List",
+                href: "/app/customers",
+              },
+              {
+                label: isCreate ? "Create Customer" : `Customer: ${projectId}`,
+              },
+            ]}
+          />
+        </div>
+        {/* {query.isLoadingError ? (
+          <>
+            <X className="text-destructive mx-auto w-24 h-24" />
+            <h2 className="text-center text-destructive my-5">
+              Project not found
+            </h2>
+          </>
+        ) : ( */}
+        <>
+          <div className="w-full bg-orange-500 p-3 rounded-t-lg">
+            {" "}
+            <p className=" text-white ml-4 font-semibold">Contact</p>
+          </div>
+          <div className="grid grid-cols-3 gap-y-3 gap-x-5 mb-10 mt-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Customer Name</FormLabel>
+                  <FormControl>
+                    <Input {...field} />
+                  </FormControl>
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Address</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Zip Code</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Tax ID</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Branch</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Branch Code</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Branch Name</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Account Number</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Account Type</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Credit Date</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Selling Price</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Vat</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Unit</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Income Account</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <div />
+          </div>
+          <div className="w-full bg-orange-500 p-3 rounded-t-lg">
+            {" "}
+            <p className=" text-white ml-4 font-semibold">Contact</p>
+          </div>
+          <div className="grid grid-cols-3 gap-y-3 gap-x-5 mb-10 mt-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Credit Date</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Selling Price</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>VAT</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <div className="w-full bg-orange-500 p-3 rounded-t-lg">
+            {" "}
+            <p className=" text-white ml-4 font-semibold">Address</p>
+          </div>
+          <div className="grid grid-cols-3 gap-y-3 gap-x-5 mb-10 mt-4">
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Address</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>Zip Code</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+            <FormField
+              control={form.control}
+              name="name"
+              render={({ field }) => (
+                <FormItem>
+                  <FormLabel>VAT</FormLabel>
+                  <Input {...field} />
+                  <FormMessage />
+                </FormItem>
+              )}
+            />
+          </div>
+          <Separator />
+          <div className="w-full bg-orange-500 p-3 rounded-t-lg">
+            <p className=" text-white ml-4 font-semibold">Note</p>
+          </div>
+          <FormField
+            control={form.control}
+            name="detail"
+            render={({ field }) => (
+              <FormItem className="col-span-3 mb-10">
+                <FormControl>
+                  <Textarea
+                    {...field}
+                    value={field.value ?? ""}
+                    className=" h-32"
+                  />
+                </FormControl>
+                <FormMessage />
+              </FormItem>
+            )}
+          />
+          <div className="w-full bg-orange-500 p-3 rounded-t-lg mb-6">
+            <p className=" text-white ml-4 font-semibold">Attachment</p>
+          </div>
+          <div className=" h-8 border-dotted bg-slate-300 flex items-center justify-center">
+            <FileUploadDropzone
+              {...fileUploadProps}
+              multiple={false}
+              accept={{
+                "image/png": [".png"],
+                "image/jpg": [".jpg"],
+                "image/jpeg": [".jpeg"],
+                "image/webp": [".webp"],
+              }}
+            />
+          </div>
+          <div className="flex justify-end items-center gap-5  mt-10">
+            {isCreate ? (
+              <Button type="submit" disabled={!form.formState.isDirty}>
+                Create
+              </Button>
+            ) : (
+              <>
+                {isEdit ? (
+                  <>
+                    <Button type="button" onClick={() => setIsEdit(false)}>
+                      Cancel
+                    </Button>
+                    <Button type="submit" disabled={!form.formState.isDirty}>
+                      Save
+                    </Button>
+                  </>
+                ) : (
+                  <Button type="button" onClick={() => setIsEdit(true)}>
+                    Edit
+                  </Button>
+                )}
+              </>
+            )}
+          </div>
+        </>
+        {/* )} */}
+      </DashboardFormContainer>
+    </Form>
+  );
+}
