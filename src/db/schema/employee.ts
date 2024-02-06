2d81f5aa3019fa331e93b7ba83b090349ac18438
Rattapon Apiratjit <krtp.apirat@gmail.com>
2024-02-06 13:55:36 +0700

    add employee db schema

---
+++ src/db/schema/employee.ts
@@ -0,0 +1,26 @@
+import { sql } from "drizzle-orm";
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core";
+import { users } from "./auth";
+
+export const employees = pgTable("employee", {
+  id: uuid("id").notNull().primaryKey(),
+  // Information
+  saleNo: text("sale_no").notNull(),
+  name: text("name").notNull(),
+  email: text("email"),
+  phoneNumber: text("phone_number"),
+  image: text("image"),
+  // Metadata
+  createdAt: timestamp("created_at", { mode: "date" })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  createdBy: text("created_by").references(() => users.id, {
+    onDelete: "set null",
+  }),
+  updatedAt: timestamp("updated_at", { mode: "date" })
+    .notNull()
+    .default(sql`CURRENT_TIMESTAMP`),
+  updatedBy: text("updated_by").references(() => users.id, {
+    onDelete: "set null",
+  }),
+});
